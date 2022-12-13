555c829d277ad9550b1bf4f3e10454c7fb198c3c
purple360 <64763854+purple360@users.noreply.github.com>
2022-12-13 12:51:58 +0000

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-	int str[] = {72, 111, 108, 98, 101, 114, 116, 111, 110};
+	int str[] = {95, 80, 117, 116, 67, 104, 65, 82};
 	int count, sz;
 
 	sz = sizeof(str) / sizeof(int);
